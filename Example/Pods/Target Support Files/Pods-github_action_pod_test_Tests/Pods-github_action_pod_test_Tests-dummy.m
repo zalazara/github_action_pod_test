35d15c261624485c4a12a57ef4d6a47ab6a46164
Alejandro Zalazar <azalazar@veritran.com>
2021-09-14 17:19:51 -0300

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-github_action_pod_test_Tests/Pods-github_action_pod_test_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_github_action_pod_test_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_github_action_pod_test_Tests
+@end
---
+++ Example/Pods/Target Support Files/github_action_pod_test/github_action_pod_test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_github_action_pod_test : NSObject
+@end
+@implementation PodsDummy_github_action_pod_test
+@end
